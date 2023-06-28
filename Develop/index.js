b31b31c106378b15dee47171bcbf52eecc48db36
Emma Butler <130527417+butlerem@users.noreply.github.com>
2023-06-27 20:57:50 -0400

    generatemarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "Name your project."
+    },{
+      type: "input",
+      name: "description",
+      message: "Describe the purpose of this project, what problem it solves, and what you learned."
+    },{
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use of the project."
+    },{
+      type: "input",
+      name: "screenshot",
+      message: "Provide the relative path to the image of a screenshot of the project."
+    },{
+      type: "input",
+      name: "link",
+      message: "Provide a URL where a user can access your deployed application."
+    },{
+      type: "input",
+      name: "credits",
+      message: "List any collaborators with links to their GitHub profiles and links to any third-party assets."
+    },{
+      type: "checkbox",
+      name: "license",
+      message: "Please select a license applicable to this project.",
+      choices: ["MIT", "APACHE2.0", "GNU GPLv3", "none"]
+    },{
+      type: "input",
+      name: "test",
+      message: "Provide exaples of how to run any tests for your application.",
+    },{
+      type: "input",
+      name: "creator",
+      message: "Provide your GitHub username.",
+      },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
+  
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating Professional README.md File...");
+    writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+  });
+}
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'APACHE2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'GNU GPLv3': 'https://www.gnu.org/licenses/gpl-3.0.en.html'
+  };
+  const link = licenseLinks[license];
+  return link ? link : '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseSections = {
+    'MIT': 'This project is licensed under the MIT License.',
+    'APACHE2.0': 'This project is licensed under the Apache License 2.0.',
+    'GNU GPLv3': 'This project is licensed under the GNU General Public License v3.0.'
+  };
+  const section = licenseSections[license];
+  return section ? section : '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
 
+## Description
+${data.description}
+
+## Usage
+${data.usage}
+
+![alt-text](${data.screenshot})
+
+${data.link}
+
+## License
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+${renderLicenseBadge(data.license)}
+
+## Credits
+${data.credits}
+
+## Testing
+${data.test}
+
+## Questions
+Please send any questions to [github/${data.creator}](https://github.com/${data.creator}).
 `;
 }
 
